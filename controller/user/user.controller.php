3a395789249d836c8c3cba537a825adc5e407ed7
bien thanh danh <thanhdanh010499@gmail.com>
2021-04-08 16:14:06 +0700

    them dc vai thu

--- controller/user/user.controller.php
+++ controller/user/user.controller.php
@@ -138,3 +138,15 @@ function checkRole()
 {
 	return sv_checkRole();
 }
+function rand_string($length){
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $size = strlen($chars);
+    $str = '';
+    for( $i = 0; $i < $length; $i++ ) {
+
+        $str .= $chars[rand(0,$size - 1)];
+
+    }
+    return $str;
+}
+?>
